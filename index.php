0bce1e620efe5c4e87b69fc8e5952bfa1fa8c46c
Moozaheed <57921369+Moozaheed@users.noreply.github.com>
2024-05-02 15:13:26 +0600

    Index file has been separated and database connection established

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include "includes/header.php"; ?>
+
+<body>
+
+    <!-- Navigation -->
+    <?php include "includes/navigation.php"; ?>
+   
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <?php include "includes/content.php"; ?>
+
+            <!-- Blog Sidebar Widgets Column -->
+            
+            <?php include "includes/sidebar.php"; ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+<?php include "includes/footer.php"; ?>
